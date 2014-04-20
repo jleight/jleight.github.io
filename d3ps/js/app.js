397f901c558ffe23b3c994aa44872c913acee2ca
Jonathon Leight <jonathon.leight@jleight.com>
2014-04-20 17:53:30 -0400

    Adding darker theme toggle.

--- d3ps/js/app.js
+++ d3ps/js/app.js
@@ -1,3 +1,11 @@
 (function (Calc) {
   Calc.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+  document.styleSheets[1].disabled = true;
+  $('body').on('click', '#style-toggle', function () {
+    $.each(document.styleSheets, function (i, e) {
+      e.disabled = !e.disabled;
+    });
+    return false;
+  });
 })(window.Calc || (window.Calc = Ember.Application.create()));
