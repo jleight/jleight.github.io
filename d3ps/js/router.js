fe1a4c81f977d6d7c7c29fa32a92c562f274c878
Jonathon Leight <jonathon.leight@jleight.com>
2014-04-21 16:27:05 -0400

    Adding documentation on how damage calculation works.

--- d3ps/js/router.js
+++ d3ps/js/router.js
@@ -1,11 +1,14 @@
 (function (Calc) {
   Calc.Router.map(function () {
     this.resource('stats', { path: '/' });
+    this.resource('calculation', { path: '/calculation' });
   });
 
   Calc.StatsRoute = Ember.Route.extend({
-  	model: function () {
+    model: function () {
       return this.store.find('character', 1);
-  	}
+    }
+  });
+  Calc.CalculationRoute = Ember.Route.extend({
   });
 })(window.Calc);
